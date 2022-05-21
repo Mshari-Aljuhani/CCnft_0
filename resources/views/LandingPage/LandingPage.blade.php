b9ebeeca0d0c11f921caa85b4c3031ab24bc517e
Meshari Aljohani <mshari.aljuhani0@gmail.com>
2022-05-21 17:20:19 +0300

    initial adding vid

--- resources/views/LandingPage/LandingPage.blade.php
+++ resources/views/LandingPage/LandingPage.blade.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include("LandingPage.components.head")
+    <div class="teaser-cont">
+        <video  id="videoBG"  autoplay muted loop>
+            <source src="{{asset('video/teaser1.mp4')}}" type="video/mp4">
+        </video>
+        <video  id="videoBG2"  autoplay muted loop>
+            <source src="{{asset('video/teaser2.mp4')}}" type="video/mp4">
+        </video>
+    </div>
 <body class="container-fluid">
-<div>
     @include('LandingPage.components.header')
-</div>
 <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
 <div id="preloader"></div>
--- resources/views/LandingPage/components/header.blade.php
+++ resources/views/LandingPage/components/header.blade.php
@@ -1,20 +1,21 @@
+<!-- The video -->
+
+
 <!-- ======= Header ======= -->
 <nav class="navbar fixed-top">
     <div class="container-fluid row">
-        <div class="col">
-            <video id="myVideo"  autoplay muted loop>
-                <source src="{{asset('video/favicon.mp4')}}" type="video/mp4">
-            </video>
+        <div class="col myLogo" style="text-align: center">
+            <img id="myVideo" src="{{asset('video/favicon.gif')}}" alt="Logo"/>
         </div>
-        <div class=" col iconsContainer col">
-            <ul class="position-relative top-0 start-50 translate-middle-x">
-                <li class="icon">
+        <div class=" col iconsContainer" style="text-align: center">
+            <ul>
+                <li>
                     <a href="https://www.instagram.com/charcoalchroniclesnft/" target="_blank"> <img class="socialmedia" src="{{asset('img/IG.png')}}" alt="IG"/> </a>
                 </li>
-                <li class=" icon">
-                    <a href="https://dsc.gg/charcoal-chronicles-w" target="_blank" ><img class="discord px-sm-5" src="{{asset('img/DISCORD.png')}}" alt="Discord"/></a>
+                <li>
+                    <a href="https://dsc.gg/charcoal-chronicles-w" target="_blank" ><img class="discord" src="{{asset('img/DISCORD.png')}}" alt="Discord"/></a>
                 </li>
-                <li class="icon">
+                <li>
                     <a href="https://twitter.com/charcoalchronft" target="_blank"><img class="socialmedia" src="{{asset('img/TWITTER.png')}}" alt="Twiiter"/></a>
                 </li>
             </ul>
